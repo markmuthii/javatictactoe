edddb2543fc3324949333a70c1712533f09a8f0b
markmuthii <mark.muthii@strathmore.edu>
2019-02-26 02:34:35 +0300

    f

--- src/tictactoe/TicTacToe.java
+++ src/tictactoe/TicTacToe.java
@@ -19,6 +19,7 @@ public class TicTacToe extends Application {
         Scene scene = new Scene(root);
         
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.setTitle("Tic Tac Toe");
         stage.show();
     }
