57382d835e386538289adb9b375376190b2f9d7d
markmuthii <mark.muthii@strathmore.edu>
2019-02-26 02:27:06 +0300

    t

--- src/tictactoe/TicTacToeController.java
+++ src/tictactoe/TicTacToeController.java
@@ -49,7 +49,17 @@ public class TicTacToeController implements Initializable {
             win5 = new Vector<>(), 
             win6 = new Vector<>(), 
             win7 = new Vector<>(), 
-            win8 = new Vector<>();
+            win8 = new Vector<>(),
+            win01 = new Vector<>(), 
+            win02 = new Vector<>(), 
+            win03 = new Vector<>(), 
+            win04 = new Vector<>(), 
+            win05 = new Vector<>(), 
+            win06 = new Vector<>(), 
+            win07 = new Vector<>(), 
+            win08 = new Vector<>(),
+            win09 = new Vector<>(),
+            win10 = new Vector<>();
     
     
     @FXML
@@ -57,13 +67,13 @@ public class TicTacToeController implements Initializable {
     @FXML
     private Button levelSelectBtn, modeSelectBtn, startBtn, resetBtn, exitBtn;
     @FXML
-    private AnchorPane levelAnchorPane, modeAnchorPane, playerNamePane, gameAnchorPane;
+    private AnchorPane levelAnchorPane, modeAnchorPane, playerNamePane, l3gameAnchorPane, l4gameAnchorPane;
     @FXML
     private TextField player1Input, player2Input;
     @FXML
-    private GridPane playGrid;
+    private GridPane l3playGrid, l4playGrid;
     @FXML
-    private Label p1ScoreField, p2ScoreField, p1NameField, p2NameField;
+    private Label p1ScoreField, p2ScoreField, l3p1ScoreField, l3p2ScoreField, p1NameField, p2NameField, l3p1NameField, l3p2NameField;
     
     @FXML
     private void levelSelectBtnAction(ActionEvent event) {
@@ -112,9 +122,21 @@ public class TicTacToeController implements Initializable {
         p1Name = p1.getName();
         p2Name = p2.getName();
 //        System.out.println("Game started! p1: " + p1Name + " p2: " + p2Name);
-        p1NameField.setText(p1Name);
-        p2NameField.setText(p2Name);
-        gameAnchorPane.setVisible(true);
+        if(level == "4"){
+            p1NameField.setText(p1Name);
+            p2NameField.setText(p2Name);
+        } else {
+            l3p1NameField.setText(p1Name);
+            l3p2NameField.setText(p2Name);
+        }
+        if(level == "3"){
+            l3gameAnchorPane.setVisible(true);
+            l3gameAnchorPane.setDisable(false);
+        }
+        else if(level == "4"){
+            l4gameAnchorPane.setVisible(true);
+            l4gameAnchorPane.setDisable(false);
+        }
         playerNamePane.setVisible(false);
     }
     
@@ -132,7 +154,10 @@ public class TicTacToeController implements Initializable {
             Integer rowIndex = (GridPane.getRowIndex(source) == null) ? 0 : (GridPane.getRowIndex(source));
 
     //      draw symbol (X or O)
-            playGrid.add(sym, colIndex, rowIndex);
+            if(level == "3")
+                l3playGrid.add(sym, colIndex, rowIndex);
+            else
+                l4playGrid.add(sym, colIndex, rowIndex);
             
             GridPane.setHalignment(sym, HPos.CENTER);
 //            System.out.println("Pane clicked! Row: " + rowIndex + ", Col: " + colIndex);
@@ -141,14 +166,7 @@ public class TicTacToeController implements Initializable {
             changePlayer();
             moves++;
             gameState();
-            return;
         }
-        Alert gameWonAlert = new Alert(Alert.AlertType.INFORMATION);
-        gameWonAlert.setTitle("Game Won!");
-        gameWonAlert.setHeaderText(null);
-        gameWonAlert.setContentText(playerWon + " has won the game!");
-        System.out.println("Game won by " + playerWon);
-        gameWonAlert.showAndWait();
         
     }
     
@@ -164,7 +182,7 @@ public class TicTacToeController implements Initializable {
     
     @FXML
     private void resetBoard(){
-        playGrid.getChildren().forEach((stackPane) -> {
+        l3playGrid.getChildren().forEach((stackPane) -> {
             System.out.println(stackPane);
             
             
@@ -196,30 +214,69 @@ public class TicTacToeController implements Initializable {
     }
     
     private void gameState(){
-        if(indicesX.containsAll(win1) || indicesX.containsAll(win2) || indicesX.containsAll(win3) || indicesX.containsAll(win4) || indicesX.containsAll(win5) || indicesX.containsAll(win6) || indicesX.containsAll(win7) || indicesX.containsAll(win8)){
-            isGameWon = true;
-            playerWon = p1Name;
-            p1Score++;
-            changeScore(1);
-        } else if(indicesO.containsAll(win1) || indicesO.containsAll(win2) || indicesO.containsAll(win3) || indicesO.containsAll(win4) || indicesO.containsAll(win5) || indicesO.containsAll(win6) || indicesO.containsAll(win7) || indicesO.containsAll(win8)){
-            isGameWon = true;
-            playerWon = p2Name;
-            p2Score++;
-            changeScore(2);
+        if(level == "3"){
+            if(indicesX.containsAll(win1) || indicesX.containsAll(win2) || indicesX.containsAll(win3) || indicesX.containsAll(win4) || indicesX.containsAll(win5) || indicesX.containsAll(win6) || indicesX.containsAll(win7) || indicesX.containsAll(win8)){
+                isGameWon = true;
+                playerWon = p1Name;
+                p1Score++;
+                changeScore(1);
+                showGameWon();
+                resetBoard();
+            } else if(indicesO.containsAll(win1) || indicesO.containsAll(win2) || indicesO.containsAll(win3) || indicesO.containsAll(win4) || indicesO.containsAll(win5) || indicesO.containsAll(win6) || indicesO.containsAll(win7) || indicesO.containsAll(win8)){
+                isGameWon = true;
+                playerWon = p2Name;
+                p2Score++;
+                changeScore(2);
+                showGameWon();
+                resetBoard();
+            }
+        } else if (level == "4"){
+            if(indicesX.containsAll(win01) || indicesX.containsAll(win02) || indicesX.containsAll(win03) || indicesX.containsAll(win04) || indicesX.containsAll(win05) || indicesX.containsAll(win06) || indicesX.containsAll(win07) || indicesX.containsAll(win08) || indicesX.containsAll(win09) || indicesX.containsAll(win10)){
+                isGameWon = true;
+                playerWon = p1Name;
+                p1Score++;
+                changeScore(1);
+                showGameWon();
+                resetBoard();
+            } else if(indicesO.containsAll(win01) || indicesO.containsAll(win02) || indicesO.containsAll(win03) || indicesO.containsAll(win04) || indicesO.containsAll(win05) || indicesO.containsAll(win06) || indicesO.containsAll(win07) || indicesO.containsAll(win08) || indicesO.containsAll(win09) || indicesO.containsAll(win10)){
+                isGameWon = true;
+                playerWon = p2Name;
+                p2Score++;
+                changeScore(2);
+                showGameWon();
+                resetBoard();
+            }
         }
+    }
+    
+    private void showGameWon(){
+        Alert gameWonAlert = new Alert(Alert.AlertType.INFORMATION);
+        gameWonAlert.setTitle("Game Won!");
+        gameWonAlert.setHeaderText(null);
+        gameWonAlert.setContentText(playerWon + " has won the game!");
+        System.out.println("Game won by " + playerWon);
+        gameWonAlert.showAndWait();
     }
     
     private void changeScore(int p){
         switch(p){
             case 1:
-                p1ScoreField.setText(p1Score + "");
+                if(level == "3")
+                    l3p1ScoreField.setText(p1Score + "");
+                else
+                    p1ScoreField.setText(p1Score + "");
                 break;
             case 2:
-                p2ScoreField.setText(p2Score + "");
+                if(level == "3")
+                    l3p2ScoreField.setText(p2Score + "");
+                else
+                    p2ScoreField.setText(p2Score + "");
                 break;
             case 3:
                 p1ScoreField.setText(p1Score + "");
+                l3p1ScoreField.setText(p1Score + "");
                 p2ScoreField.setText(p2Score + "");
+                l3p2ScoreField.setText(p2Score + "");
                 break; 
         }
     }
@@ -230,7 +287,7 @@ public class TicTacToeController implements Initializable {
         levelSelect.setItems(levelOptions);
         modeSelect.setItems(modeOptions);
         
-//      winning combinations
+//      winning combinations for l3
         win1.add("00");
         win1.add("01");
         win1.add("02");
@@ -262,6 +319,57 @@ public class TicTacToeController implements Initializable {
         win8.add("20");
         win8.add("11");
         win8.add("02");
+        
+//      winning combinations for l4
+        win01.add("00");
+        win01.add("01");
+        win01.add("02");
+        win01.add("03");
+        
+        win02.add("10");
+        win02.add("11");
+        win02.add("12");
+        win02.add("13");
+        
+        win03.add("20");
+        win03.add("21");
+        win03.add("22");
+        win03.add("23");
+        
+        win04.add("00");
+        win04.add("10");
+        win04.add("20");
+        win04.add("30");
+        
+        win05.add("01");
+        win05.add("11");
+        win05.add("21");
+        win05.add("31");
+        
+        win06.add("02");
+        win06.add("12");
+        win06.add("22");
+        win06.add("32");
+        
+        win07.add("00");
+        win07.add("11");
+        win07.add("22");
+        win07.add("33");
+        
+        win08.add("03");
+        win08.add("12");
+        win08.add("21");
+        win08.add("30");
+        
+        win09.add("30");
+        win09.add("31");
+        win09.add("32");
+        win09.add("33");
+        
+        win10.add("03");
+        win10.add("13");
+        win10.add("23");
+        win10.add("33");
     }    
     
 }
